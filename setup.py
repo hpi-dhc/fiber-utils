3c34bca4d936a9418fa0f9930d3d6d35d664aa97
Tom Martensen <mail@tommartensen.de>
2020-02-13 17:46:17 +0100

    fix tests

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     include_package_data=True,
     keywords='fiberutils',
     name='fiberutils',
-    packages=find_packages(include=['fiberutils']),
+    packages=find_packages(include=['fiberutils', 'fiberutils.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
